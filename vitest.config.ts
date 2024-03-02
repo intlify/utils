700bb49f32dc689f19db7d9d6d43cfa1367f304c
kazuya kawaguchi <kawakazu80@gmail.com>
2024-03-02 18:41:31 +0900

    chore: upgrade vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
+    exclude: [...configDefaults.exclude, '**/playground/**'],
   },
 })
