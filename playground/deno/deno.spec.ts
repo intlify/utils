67828f01ee2073b292a1ef42a747fefe5242d825
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-09 16:17:49 +0900

    chore: use deno from node_modules

--- playground/deno/deno.spec.ts
+++ playground/deno/deno.spec.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.204.0/assert/mod.ts'
+import { assertEquals } from '@std/assert'
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
