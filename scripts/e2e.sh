3786d17d598fdeb0be7f1b315d1ca454680f977c
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-09 15:56:33 +0900

    chore: fix more e2e script

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -5,14 +5,5 @@ set -e
 # Install playwright
 pnpm exec playwright install
 
-# Pack packages
-pnpm pack
-
-# TODO(kazupon): Remove this when jsr supports pnpm workspace
-# Replace deps
-pnpx tsx ./scripts/replaceDeps.ts
-
-pnpm install
-
 # just do e2e!
 pnpm run test:e2e
